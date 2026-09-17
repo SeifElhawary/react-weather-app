9119d9942ba754a946a43d9fac3384fc7cecb855
Seif Haytham Kamal <seif.haitham.bis512500426@commerce.helwan.edu.eg>
2026-09-17 04:14:45 +0300

    Refactor Weather component for improved readability

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -10,7 +10,7 @@ import { LoadingContext } from '../../context/LoadingContext';
 
 function Weather() {
   const { inputValue } = useContext(LoadingContext);
-  const data = useFetch(inputValue?.trim());
+  const data = useFetch(inputValue);
 
   let temp = Cloud;
 
